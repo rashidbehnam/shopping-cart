55b62759083a912468aa091a6b51cdf5985796a1
rashidbehnam <rashidbehnam2012@gmail.com>
2023-12-15 09:47:55 +0330

    home page modified

--- src/App.jsx
+++ src/App.jsx
@@ -127,7 +127,7 @@ function App() {
                     className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0 md:text-blue-700"
                     aria-current="page"
                   >
-                    Home
+                    Home page
                   </Link>
                 </li>
                 <li>
